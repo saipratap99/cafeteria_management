2bb8330c2063b683bb0da8de467c9ab596a12f4e
saipratap99 <122005067@sastra.ac.in>
2020-04-15 20:11:10 +0530

    Users controller ,model and migrations are done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_062940) do
+ActiveRecord::Schema.define(version: 2020_04_15_143718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 2020_04_10_062940) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status", default: "being_created"
     t.datetime "ordered_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "role"
+    t.string "password_digest"
+    t.string "password_confirmation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
