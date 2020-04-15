8bdb5bf412e9baf1e809e29f6e680ba3afeedfee
saipratap99 <122005067@sastra.ac.in>
2020-04-15 20:29:51 +0530

    Solved repeated code in all erb and added signUp form design

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :menus, :menu_items, :orders, :order_items
+  resources :menus, :menu_items, :orders, :order_items, :users
   get "/cart", to: "orders#cart", as: "cart"
   get "/pending_orders", to: "orders#pending_orders", as: "pending_orders"
 end
