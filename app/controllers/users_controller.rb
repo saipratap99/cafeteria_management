211c77be8b74b12275cc6690155d65432d465e50
saipratap99 <122005067@sastra.ac.in>
2020-04-15 20:59:17 +0530

    Now user can creste account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    password_confirmation = params[:password_confirmation]
+
+    user = User.new(name: name, email: email, role: "customer", password: password, password_confirmation: password_confirmation)
+    if user.password_confirmation
+      user.save!
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
+  end
 end
