bf18f47823e4b6fd3391bbebdb8cc059a93dec35
saipratap99 <122005067@sastra.ac.in>
2020-05-12 10:36:56 +0530

    Added jQuery and added same animation functions in app.js using jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +15,28 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+$(document).ready(function () {
+
+  $(".cafeteria-navbarHeading > .container").click(
+    function () {
+      $(this).toggleClass("change")
+      $(".cafeteria-navbarAllButtons").toggle(500);
+    }
+  );
+
+  $(".menuButton").click(function () {
+    $(".cafeteria-menu").toggle(500);
+  });
+
+  $("#menu-name").change(function () {
+    if ($(this).val() == "Others") {
+      $("#new_menu_name").show(300, function () {
+        $(".new-menu-name-edit").css({ "display": "block" });
+      });
+    }
+    else {
+      $("#new_menu_name").hide(300);
+    }
+  });
+
+});
