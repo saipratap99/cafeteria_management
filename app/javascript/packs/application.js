2860039e784b2103669c7f686901ee8548b9222a
saipratap99 <122005067@sastra.ac.in>
2020-06-24 08:51:35 +0530

    Changed display to none for mobile menus button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,14 +15,12 @@ require("jquery")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
 $(document).ready(function () {
-
-  $(document).on("click", ".cafeteria-navbarHeading > .container",
-    function () {
-      $(this).toggleClass("change")
-      $(".cafeteria-navbarAllButtons").toggle(300);
-
-    });
+  $(document).on("click", ".cafeteria-navbarHeading > .container", function () {
+    $(this).toggleClass("change");
+    $(".cafeteria-navbarAllButtons").toggle(300);
+  });
 
   $(document).on("click", ".menuButton", function () {
     $(".cafeteria-menu").toggle(300);
@@ -31,10 +29,9 @@ $(document).ready(function () {
   $(document).on("change", "#menu-name", function () {
     if ($(this).val() == "Others") {
       $("#new_menu_name").show(300, function () {
-        $(".new-menu-name-edit").css({ "display": "block" });
+        $(".new-menu-name-edit").css({ display: "block" });
       });
-    }
-    else {
+    } else {
       $("#new_menu_name").hide(300);
     }
   });
